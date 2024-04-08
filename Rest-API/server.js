82f69c729d64a94963c8e7b661e5037df1be8d66
Thisanduedirisinghe <thisandu.n.edirisinghe@gmail.com>
2024-04-08 23:02:57 +0530

    first commit
    node init

---
+++ Rest-API/server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json())
+
+const convertParams = (req, res, next) => {
+    const { id } = req.params;
+    req.params.id = parseInt(id);
+    next();
+};
+
+app.get('/', (req, res) => {
+    res.send('Success!')
+});
+
+let blogs = [];
+
+app.get("/blog", (req, res) => {
+    res.json(blogs)
+});
+
+app.post("/blog",(req,res) => {
+    blogs.push({ id: blogs.length + 1, ...req.body });
+    res.status(201).send({ message: "ok!" } );
+});
+
+app.put("/blog/:id" , convertParams, (req,res) => {
+    const { id } = req.params;
+    const index = blogs.findIndex((item) => item.id === id)
+    blogs[index] = req.body;
+    res.send(blogs[index]);
+});
+
+app.get("/blog/:id" , convertParams, (req, res) => {
+    const { id } = req.params;
+    const index = blogs.findIndex((item) => item.id === id);
+    res.send(blogs[index])
+});
+app.delete("/blog/:id" , convertParams, (req, res) => {
+    const { id } = req.params;
+    const index = blogs.findIndex((item) => item.id === id);
+
+
+    if(index === -1) {
+        res.status(404).send({ message: "Not Found!"})
+    }
+
+    blogs.splice(index, 1);
+    res.send({ message: "Deleted!" })
+})
+
+app.listen(PORT, () => console.log(`App is listening on PORT: ${PORT}`));
